b2a46727f6b729b7ee4e0ae642a797bc947bba55
Alexis <alexis.sainsard@gmail.com>
2020-12-10 11:27:15 +0100

    no message

--- TP_AI_Project/Assets/Teams/ExampleTeam/Tasks/isObstacleInMyDirection.cs
+++ TP_AI_Project/Assets/Teams/ExampleTeam/Tasks/isObstacleInMyDirection.cs
@@ -9,21 +9,24 @@ namespace TeamImpact
 		
 		public float maxDistanceToCheck = 10f;
 		public SharedVector3 currentPosition;
-		public SharedVector2 velocity;
+		public SharedVector3 closestWP;
 
+		
 		public override TaskStatus OnUpdate()
 		{
 			Vector2 startRay = new Vector2(currentPosition.Value.x, currentPosition.Value.y);
+			Vector2 dirRay = new Vector2(closestWP.Value.x, closestWP.Value.y);
+			//if (velocity.Value.magnitude == 0) return TaskStatus.Failure;
 
-			if (velocity.Value.magnitude == 0) return TaskStatus.Failure;
-
-			RaycastHit2D hit = Physics2D.Raycast(startRay, velocity.Value, maxDistanceToCheck, 12); //layer 12 = asteroids
-			Debug.DrawRay(startRay, velocity.Value.normalized * maxDistanceToCheck, Color.red);
+			LayerMask layerMask = LayerMask.GetMask("Asteroid");
+			RaycastHit2D hit = Physics2D.Raycast(startRay, dirRay, maxDistanceToCheck, layerMask); //layer 12 = asteroids
+			Debug.DrawRay(startRay, dirRay.normalized * maxDistanceToCheck, Color.red);
 
 			if (hit.collider == null)
             {
 				return TaskStatus.Failure;
             }
+			Debug.Log(hit.collider.name);
 			return TaskStatus.Success;
 		}
 	}
